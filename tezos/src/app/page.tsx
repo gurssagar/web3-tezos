f86e3cdd2785aa4c90d7731913b045efeface067
gurssagar <123202956+gurssagar@users.noreply.github.com>
2024-10-13 12:31:46 +0530

    initial comit

---
+++ tezos/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Menu from "../components/header/menu";
+import {CoverDemo} from "../components/header/head1";
+import {GlobeDemo} from "@/components/header/head2";
+export default function Home() {
+  return (
+      <>
+          <Menu/>
+          <CoverDemo/>
+          <GlobeDemo/>
+      </>);
+}
---
+++ tezos/src/components/header/menu.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Icon } from '@iconify/react';
+import { useEffect, useState } from "react";
+
+export default function Menu() {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+        const storedTheme = localStorage.getItem('theme') || 'light';
+        setTheme(storedTheme);
+    }, [setTheme]);
+
+    const toggleTheme = () => {
+        const newTheme = theme === "dark" ? "light" : "dark";
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+    };
+
+    if (!mounted) {
+        return null;
+    }
+
+
+    return (
+        <>
+            <div className="flex p-4 bg-black justify-between items-center">
+                <Image src={"/image(1).webp"} alt={"Logo"} width={200} height={50}/>
+                <NavigationMenu>
+                    <NavigationMenuList className="flex gap-4 items-center">
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className="text-white hover:text-gray-300">Features</NavigationMenuLink>
+
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href="/how-it-works" legacyBehavior passHref>
+                                <NavigationMenuLink className="text-white hover:text-gray-300">
+                                    How it Works
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+                <NavigationMenu>
+                    <NavigationMenuList className="flex gap-4 items-center">
+                    <NavigationMenuItem>
+                            <Link href="/sign-in" legacyBehavior passHref>
+                                <NavigationMenuLink className="text-white hover:text-gray-300">
+                                    Sign In
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                        <Button
+                            variant="ghost"
+                            size="icon"
+                            onClick={toggleTheme}
+                        >
+                            {theme === "dark" ? (
+                                <>
+                                    <Icon icon="solar:sun-bold" width="24" height="24"  />
+
+                                </>
+                            ) : (
+                                <>
+                                    <Icon icon="line-md:moon-filled" width="24" height="24" />
+
+                                </>
+                            )}
+                        </Button>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+        </>
+    );
+}
